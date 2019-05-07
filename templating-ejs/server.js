e07a1c4976a74ad5b550f97c371e4ffeba7ea93e
Kayley <kayley.luftig@gmail.com>
2019-05-07 22:30:59 +0000

    updated to pass data through views files and looped over data and added a single variable

--- templating-ejs/server.js
+++ templating-ejs/server.js
@@ -11,9 +11,9 @@ app.set('view engine', 'ejs');
 //index page
 app.get('/', function(req, res) {
   var drinks = [
-    { name: 'Bloody Mary', drunknes: 3 },
-    { name: 'Martini', drunknes: 5 },
-    { name: 'Scotch', drunknes: 10 },
+    { name: 'Bloody Mary', drunkness: 3 },
+    { name: 'Martini', drunkness: 5 },
+    { name: 'Scotch', drunkness: 10 },
   ];
   var tagline = "Any code of your own that you havent looked at for six or more months might as well have been written by someone else";
 
